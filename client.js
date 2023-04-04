384d6139a80d71006d94e6ede6ba51bd5928afba
leehm123456 <banana8967@naver.com>
2023-04-04 23:09:29 +0900

    client 전역변수로 선언

---
+++ client.js
@@ -0,0 +1,13 @@
+import StompJs from "@stomp/stompjs";
+
+export const client = new StompJs.Client({
+  brokerURL: "ws://10.0.2.2:8080/ws",
+  forceBinaryWSFrames: true,
+  appendMissingNULLonIncoming: true,
+  debug: function (str) {
+    console.log(str);
+  },
+  reconnectDelay: 5000,
+  heartbeatIncoming: 4000,
+  heartbeatOutgoing: 4000,
+});
